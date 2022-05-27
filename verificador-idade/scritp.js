2db7c2cd8c6a05d128dbe67d5f37e14d8595ec78
Danúbia Carvalho <74597179+dancarvalho09@users.noreply.github.com>
2022-05-27 17:29:10 -0300

    Update scritp.js

--- verificador-idade/scritp.js
+++ verificador-idade/scritp.js
@@ -9,6 +9,8 @@ function verificar(){
     var idade = ano_atual - Number(form_ano.value)
     var genero = ''
     var img = window.document.getElementById('imagem')
+    var section = window.document.getElementById('section')
+    
 
 
     if (form_ano.value.length == 0 || form_ano.value > ano_atual) {
@@ -18,24 +20,44 @@ function verificar(){
     if (form_sexo[0].checked) {
         genero = 'Homem'
         
-        
 
         if(idade >=0 && idade < 5) {
             img.src = 'imagens/baby-masc.jpg'
-            img.style.backgroundColor = 'dodgerblue'
+            section.style.backgroundColor = '#cbe8ef'
         } else if (idade <= 13) {
             img.src = 'imagens/crianca-masc.jpg'
+            section.style.backgroundColor = '#cbe8ef'
         } else if (idade <= 25) {
             img.src = 'imagens/jovem-masc.jpg'
+            section.style.backgroundColor = '#cbe8ef'
         } else if (idade < 60) {
             img.src = 'imagens/adulto-masc.jpg'
+            section.style.backgroundColor = '#cbe8ef'
         } else {
             img.src = 'imagens/idoso-masc.jpg'
+            section.style.backgroundColor = '#cbe8ef'
         }
+        
+    } else {
+        genero = 'Mulher'
 
+        if(idade >=0 && idade < 5) {
+            img.src = 'imagens/baby-fem.jpg'
+            section.style.backgroundColor = '#ffd6f9'
+        } else if (idade <= 13) {
+            img.src = 'imagens/crianca-fem.jpg'
+            section.style.backgroundColor = '#ffd6f9'
+        } else if (idade <= 25) {
+            img.src = 'imagens/jovem-fem.jpg'
+            section.style.backgroundColor = '#ffd6f9'
+        } else if (idade < 60) {
+            img.src = 'imagens/adulto-fem.jpg'
+            section.style.backgroundColor = '#ffd6f9'
+        } else {
+            img.src = 'imagens/idoso-fem.jpg'
+            section.style.backgroundColor = '#ffd6f9'
+        }  
     }
 
-
-
-    resultado.innerHTML = `Sua idade é ${idade} anos.`
+    resultado.innerHTML = `<strong>${genero} com ${idade} anos.</strong>`
 }
